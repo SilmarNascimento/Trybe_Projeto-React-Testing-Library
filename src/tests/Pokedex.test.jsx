7de94effc624f8d370a8319cc46c96e57ca7e9cb
Shanloo <silmarfnascimento@gmail.com>
2023-03-28 19:10:12 -0300

    Feat: requisito 05

--- src/tests/Pokedex.test.jsx
+++ src/tests/Pokedex.test.jsx
@@ -3,7 +3,6 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import { NotFound, Pokedex } from '../pages';
 import pokemonList from '../data';
 
 describe('Testa o componente <Pokedex />', () => {
@@ -41,7 +40,6 @@ describe('Testa o componente <Pokedex />', () => {
   test('Verifica se ao clicar no último pokémon, o próximo volta ao primeiro', () => {
     renderWithRouter(<App />);
     const arrayId = pokemonList.map((poke) => poke.id);
-    console.log(arrayId);
 
     const firstPokemon = screen.getByRole('img', {
       name: /pikachu/i,
@@ -88,5 +86,67 @@ describe('Testa o componente <Pokedex />', () => {
     const nTypes = types.filter((item, index) => types.indexOf(item) === index).length;
 
     expect(filterBtn.length).toBe(nTypes);
+  });
+
+  test('Verifica se ao selecionar o filtro só aparecem os Pokémons filtrados', () => {
+    renderWithRouter(<App />);
+    const filterAll = screen.getByRole('button', {
+      name: /all/i,
+      class: /filter-button/i,
+    });
+    const nextBtn = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+
+    const typeArray = pokemonList.map((poke) => poke.type);
+    typeArray.forEach((type) => {
+      const filterBtn = screen.getByRole('button', {
+        name: `${type}`,
+      });
+
+      userEvent.click(filterBtn);
+
+      const quantPokemon = pokemonList.filter((poke) => poke.type === type);
+      const pokemonName = quantPokemon[0].name;
+
+      const firstFilteredPokemon = screen.getByRole('img', {
+        name: `${pokemonName} sprite`,
+      });
+
+      quantPokemon.forEach(() => {
+        userEvent.click(nextBtn);
+      });
+
+      const actualPokemon = screen.getByRole('img');
+
+      expect(firstFilteredPokemon).toEqual(actualPokemon);
+
+      userEvent.click(filterAll);
+
+      const screenPokemon = screen.getByRole('img', {
+        name: /pikachu/i,
+      });
+      expect(screenPokemon).toBeInTheDocument();
+    });
+  });
+
+  test('Verifica se o botão All reseta o filtro de Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const filterAll = screen.getByRole('button', {
+      name: /all/i,
+      class: /filter-button/i,
+    });
+    const filterBtn = screen.getAllByTestId(/pokemon-type-/i);
+    filterBtn.forEach((btn, index) => {
+      userEvent.click(filterBtn[index]);
+      userEvent.click(filterAll);
+
+      const screenPokemon = screen.getByRole('img', {
+        name: /pikachu/i,
+      });
+      expect(screenPokemon).toBeInTheDocument();
+    });
+    expect(filterAll).toBeInTheDocument();
   });
 });
